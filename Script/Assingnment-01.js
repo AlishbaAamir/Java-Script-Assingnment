8652d21196dbeb96a2ef4ce739108754d2130b76
Alishba Aamir <126007592+AlishbaAamir@users.noreply.github.com>
2023-06-04 22:08:04 +0500

    Tasks

---
+++ Script/Assingnment-01.js
@@ -0,0 +1,146 @@
+// Task 1      (6-April-2023)
+/*var zakatpercentage=0.025;
+// Take input from uer
+var input=+prompt("Calculate your Zakat amount");
+var result= zakatpercentage*input;
+alert("Zakt is " +result);/*
+
+
+// TASK=02
+/* var familyMembers = +prompt("How many members in your Family")
+var userinput = prompt("Wheat, Dates, Raisin, Balray");
+
+// Calculation
+
+var Wheat = 250
+var Dates = 2100
+var Balray = 450
+var Raisin = 2800
+
+if( userinput=="Wheat"){
+    var total = familyMembers*Wheat;
+     alert("Your Fitrah is " +total )
+}
+else if(userinput=="Dates"){
+    var total = familyMembers*Dates;
+    alert("Your Fitrah is " +total)
+}
+else if (userinput=="Raisin"){
+    var total = familyMembers*Raisin;
+    alert("Your Fitrah is "  +total)
+}
+else if (userinput=="Balray"){
+    var total = familyMembers*Balray;
+    alert("Your Fitrah is " +total)
+}
+else{
+   
+}*/
+
+
+// Task-03
+/*var secretNumber = +prompt("Guess the number Between 1 to 10")
+
+if(secretNumber==1){
+    alert("SOrry, You loss! \n Your guess is too low" )
+}
+else if (secretNumber==2){
+    alert("SOrry, You Losss! \n Your guess is too low")
+}
+else if (secretNumber==3){
+    alert( "SOrry, You loss! \n Your guess is too low")
+}
+else if (secretNumber==4){
+    alert("SOrry, You loss! \n Your guess is too low")
+}
+else if (secretNumber==5){
+    alert("CONGRATULATION")
+}
+else if (secretNumber==6){
+    alert(" SOrry, You loss! \n Your guess is too heigh")
+}
+else if (secretNumber==7){
+    alert("SOrry, You loss! \n  Your guess is too heigh")
+}
+else if (secretNumber==8){
+    alert(" SOrry, You loss! \n Your guess is too heigh")
+}
+else if (secretNumber==9){
+    alert(" SOrry, You loss! \n Your guess is too heigh")
+}
+else if (secretNumber==10){
+    alert(" SOrry, You loss! \n Your guess is too heigh")
+}
+else{
+
+}*/
+
+
+// Task-04
+/* var Name = prompt("Please Enter Your Name")
+var caps= Name.charAt(0).toUpperCase()+Name.slice(1);
+alert(caps) */
+
+// Tak-05
+
+/*var contactName=prompt("Enter your Name")
+var contactNumber=+prompt("Enter your Number")
+
+var ContactName=prompt("Enter your friend Name")
+var ContactNumber=+prompt("Enter your friend Number")
+
+var contactName1=prompt("Enter your sibling Name")
+var contactNumber1=+prompt("Enter your sibling Number")
+ alert(contactName+" "+contactNumber+ "\n"  +ContactName+" "+ContactNumber+ "\n" +contactName1+" "+contactNumber1)*/
+ 
+
+
+// Task-06
+
+ var product=["Tea", "Coffee", "Drink", "Juice", "Shakes","Beverages"]
+
+ var item=prompt("Tea \n Coffee \n Drink \n Juice \n Shake \n Beverages (1-"+product.length+")")
+ var index= parseInt(item)-1;
+
+ var removedItem=product.splice(index,1)[0]
+ alert("Removed item" +removedItem)
+
+alert("Remaining item:" +product.join(","))
+alert(" total remaining item" +products.length)
+
+
+
+
+// Task-07
+// var Nationality=prompt("Enter your nationality")
+
+
+// if(Nationality == 'Pakistani' || Nationality == 'pakistani' || Nationality== 'indian'|| Nationality=='Indian'  ){
+//  alert("You are eligible")
+// var gender=prompt("your gender")
+// var age=+prompt("your age")
+// if(gender=="male"  && age>=18){
+// alert("you are eligible")
+// }
+// if(gender=="female" && age>=18){
+//     var mstatus=prompt("you mstatus")
+//     if(mstatus=="married")
+//     alert("you are eligible")
+// }
+// else{
+//     alert("you are not eligible")
+// }
+// }
+//  else{
+//      alert("you are not eligible")
+// }
+
+
+
+
+//  // Task-08
+
+// var pakistaniTeam=["Babar", "Shadab", "Shaheen", "Fakhar", "Sarfaraz", "imad", "Haris", "Hassan"," Hasnain"," Shoaib", "Asif", "Hafeez", "Amir", "Shoail", "Shaheen" ]
+//  var teamplayer=pakistaniTeam.slice(0, 11);
+//  console.log(pakistaniTeam)
+// console.log(teamplayer)
